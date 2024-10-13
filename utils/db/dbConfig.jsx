3b724b4d3744c32710e654b2e595bbbb34accc49
Dipesh Kumar <itsdipeshk2003@gmail.com>
2024-10-14 04:45:10 +0530

    header + sidebar completed

--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -1,6 +1,5 @@
-// postgresql://trash2treasure_owner:eVGcnC9fB4hk@ep-lingering-credit-a5nojf64.us-east-2.aws.neon.tech/trash2treasure?sslmode=require
 
-import {neon} from 'neondatabase/serverless'
+import {neon} from '@neondatabase/serverless'
 import {drizzle} from 'drizzle-orm/neon-http'
 import * as schema from './schema'
 
